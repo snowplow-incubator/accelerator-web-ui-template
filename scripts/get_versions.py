93a787e1d673b733d89e5dbac1ea2d6ab621b7a4
Jack Keene <jack.keene@snowplowanalytics.com>
2023-02-13 13:46:12 +0000

    Test IO

--- scripts/get_versions.py
+++ scripts/get_versions.py
@@ -9,6 +9,8 @@ for package in temp_json["packages"]:
     res = requests.get("https://api.github.com/repos" + package["url"])
     version = res.json()["tag_name"]
     package["version"] = version
+
+print(temp_json)
 jsonFile = open("data/component_versions.json", "w+")
 jsonFile.write(json.dumps(temp_json))
 jsonFile.close()
