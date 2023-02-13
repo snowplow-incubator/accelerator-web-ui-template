2be9aa981f5d6458d3a4e75eba51586f5b02cba3
Jack Keene <jack.keene@snowplowanalytics.com>
2023-02-13 14:21:05 +0000

    Add update time

--- scripts/get_versions.py
+++ scripts/get_versions.py
@@ -1,22 +1,20 @@
 import requests
 import json
-import os
+import time
 
 with open("data/component_versions.json") as jsonFile:
     data = json.load(jsonFile)
 
 temp_json = data
 
-if os.path.exists("data/component_versions.json"):
-    os.remove("data/component_versions.json")
-else:
-    print("The file does not exist")
 
 for package in temp_json["packages"]:
     res = requests.get("https://api.github.com/repos" + package["url"])
     version = res.json()["tag_name"]
     package["version"] = version
 
+
+temp_json["last_update"] = time.time()
 print(temp_json)
 jsonFile = open("data/component_versions.json", "w+")
 jsonFile.write(json.dumps(temp_json))
